5808139500954a46f5fb59235e8df7feabe331aa
Shreya <shreyaaa03@gmail.com>
2025-06-17 20:44:52 +0530

    added react front end

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8080/api'; // or your backend's IP if running on Raspberry Pi
+const API_BASE = 'http://localhost:8080/api';
 
 export const getAllThoughts = () => axios.get(`${API_BASE}/thoughts`);
 export const createThought = (thought) => axios.post(`${API_BASE}/thoughts`, thought);
